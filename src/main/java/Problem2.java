7f201d9bca9c4160a6823d679d162e72a3da67d8
NamN2020 <72000121+NamN2020@users.noreply.github.com>
2020-10-14 11:50:35 -0700

    RemoveDuplicates complete (More comments)

--- src/main/java/Problem2.java
+++ src/main/java/Problem2.java
@@ -1,6 +1,6 @@
 /*
     Nam Nguyen
-    October 16, 2020
+    October 19, 2020
     CS 143B
     Homework_2
  */
@@ -21,19 +21,19 @@ public class Problem2 {
     // Do not change signature (function name, parameters and return type)
     // Do not use extra space for another array. Solution to be in-pace with O(1) extra memory
     public static int remove(int[] nums) {
-        if(nums == null || nums.length == 0){
+        if(nums == null || nums.length == 0){   //  filters the given Array
             return 0;
         }
 
-        int count = 1;
+        int count = 1;                          //  count starts at 1 because the Array must contain 1 element after being filtered
         int maxNum = nums[nums.length - 1];
         int idxHold = 0;
 
         for(int i = 1; i < nums.length; i ++){
             if(nums[idxHold] == nums[i]){
-                nums[i] = maxNum + 1;
+                nums[i] = maxNum + 1;           //  current value at 'i' index becomes largest number in new Array
             } else {
-                idxHold = i;
+                idxHold = i;                    //  idxHold = 'i', if they're no more duplicates of previous 'i' value
                 count ++;
             }
         }
