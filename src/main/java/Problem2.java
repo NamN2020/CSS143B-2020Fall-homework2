d9d2a64167eba9e42bfafb4c455b9ca52d801e67
NamN2020 <72000121+NamN2020@users.noreply.github.com>
2020-10-14 11:43:32 -0700

    RemoveDuplicates complete

--- src/main/java/Problem2.java
+++ src/main/java/Problem2.java
@@ -9,6 +9,13 @@ public class Problem2 {
 
     /*
     Describe your algorithm here in up to 5 sentences:
+
+    I created a for loop going through the whole array and 'i' is starting from 1. It starts at 1 because my index
+    variable holder is starting at 0 to compare the two. It compares each number to see if they're equal, if they
+    are equal than it makes the value at the 'i' index the largest number, array[array.length - 1] + 1, if they are not
+    equal than my index variable holder becomes i and the process repeats. When the process is finished all the
+    duplicate numbers will be the same number, array[array.length - 1] + 1. Lastly, I put the array through a bubble
+    sort to move all the duplicate numbers to the end.
      */
 
     // Do not change signature (function name, parameters and return type)
@@ -21,14 +28,11 @@ public class Problem2 {
         int count = 1;
         int maxNum = nums[nums.length - 1];
         int idxHold = 0;
-        //int min = nums[idxHold];
-
 
         for(int i = 1; i < nums.length; i ++){
             if(nums[idxHold] == nums[i]){
                 nums[i] = maxNum + 1;
             } else {
-                //min = nums[i];
                 idxHold = i;
                 count ++;
             }
@@ -40,7 +44,7 @@ public class Problem2 {
 
     public static void bubbleSort(int [] data){
         for (int n = 0; n < data.length - 1; n++) {
-            for (int i = 0; i < data.length - 1 - n; i++) {     //  the condition is, data.length - 1 - n, because the largest number will be at
+            for (int i = 0; i < data.length - 1 - n; i++) {     //  the condition is, data.length - 1 - n, because that's where the largest number is
                 //  the largest index every loop so this, - n, ignores the largest index for the inner for-loop
                 if (data[i] > data[i + 1]) {    //  if left side is larger than right side than swap places
                     int temp = data[i];
